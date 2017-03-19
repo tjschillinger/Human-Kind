50227eef35d10065d0442c77720e63d6a3f74fd7
Tim <tjschillinger92>
2017-03-19 11:19:23 -0600

    addition of readme

---
+++ README.mk
@@ -0,0 +1,15 @@
+Human Kind
+==================
+Human Kind encourages acts of kindness via challenges and rewards. Unfortunetely, this app does not yet exist on the market. This project, however, allowed me and my team the oppertunity to create a showcase for this app as if it existed. We poured much creativity into the project in order to position it intelligibly and convert the user.  
+
+
+![Landing Page](http://i.imgur.com/xMhjNfm.png)
+
+
+
+![FAQ Page](http://i.imgur.com/JCjn1QL.png)
+
+
+
+![Sponsor Page](http://i.imgur.com/VDuLaER.png)
+
